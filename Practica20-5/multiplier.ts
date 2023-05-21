493861e0d3583252625a35573dd6830ae271b254
Julian <julian_delisio@hotmail.com>
2023-05-21 18:28:15 -0300

    práctica typescript

--- Practica20-5/multiplier.ts
+++ Practica20-5/multiplier.ts
@@ -3,3 +3,9 @@ const multiplicator = (a: number, b: number, printText: string) => {
 }
 
 multiplicator(2, 4, "Multiplied numbers 2 and 4, the result is: ")
+
+const divide = (a: number, b: number, printText: string) => {
+    console.log(printText, a / b)
+}
+
+multiplicator(2, 4, "Divided numbers 2 and 4, the result is: ")
